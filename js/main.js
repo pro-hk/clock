b976536a8d4bfb539f145eb01cc55ec946ef8303
janghyeok <pro_hk@naver.com>
2021-12-10 16:11:23 +0900

    배경고정완료

--- js/main.js
+++ js/main.js
@@ -14,15 +14,31 @@ $.ajax({
     $.each(clockList, function (idx, item) {
       // console.log(item);
       output += `
-      <div class="section">
+      <div class="section" style="background-image:url(${item.bg})">
         <div class="info">
-          <p class="category">${item.category}</p>
-          <h2 class="title">${item.title}</h2>
-          <p class="depth">${item.depth}MM</p>
-          <p class="price">CHF ${item.price}</p>
+          <p class="category" data-splitting>${item.category}</p>
+          <h2 class="title" data-splitting>${item.title}</h2>
+          <p class="depth" data-splitting><strong>${item.depth}</strong> mm</p>
+          <p class="price" data-splitting>CHF ${item.price}</p>
         </div>
       </div>`;
     });
     main.html(output);
+    Splitting();
+    $("#main").fullpage({
+      scrollBar: true,
+      onLeave: function (origin, destination, direction) {
+        gsap.from(`.section:nth-child(${destination.index + 1}) .char`, {
+          y: -200,
+          opacity: 0,
+          ease: "bounce",
+          duration: 1.5,
+          stagger: {
+            each: 0.01,
+            from: "random",
+          },
+        });
+      },
+    });
   },
 });
